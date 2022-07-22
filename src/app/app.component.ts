c1b7ed957c826a73e7b87afd51e801e1a824b059
Gary Großgarten <8986373+garygrossgarten@users.noreply.github.com>
2022-07-22 12:03:54 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 
+export function media(query: string): Observable<boolean> {
+  const mediaQuery = window.matchMedia(query);
+  return fromEvent<MediaQueryList>(mediaQuery, 'change').pipe(
+    startWith(mediaQuery),
+    map((list: MediaQueryList) => list.matches)
+  );
+}
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div *ngIf="sm$ | async">sm</div>
+    <div *ngIf="md$ | async">md</div>
+    <div *ngIf="lg$ | async">lg</div>
+    <div *ngIf="xl$ | async">xl</div>
+    <div *ngIf="xl2$ | async">2xl</div>
+  `,
 })
 export class AppComponent {
-  title = 'media-query';
+  sm$ = media(`(max-width: 767px)`);
+  md$ = media(`(min-width: 768px) and (max-width: 1023px)`);
+  lg$ = media(`(min-width: 1024px) and (max-width: 1279px)`);
+  xl$ = media(`(min-width: 1280px) and (max-width: 1535px)`);
+  xl2$ = media(`(min-width: 1536px)`);
 }
